4d47acf46063fabbcfb9bc6754ecf0b32d6d6035
MondaSamuel <samwelmonda48@gmail.com>
2025-07-12 00:10:10 +0300

    dd

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,6 @@
 import logging
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User as User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.exceptions import ValidationError
